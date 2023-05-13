db6e5633a39e80b14603d749d7bd37b1da49b05a
Mac <MAC@MacdeMacBook-Pro.local>
2023-05-13 15:20:38 +0800

    Use case 1 changed from string based to json module based.

--- DatabaseComunicator.py
+++ DatabaseComunicator.py
@@ -50,9 +50,11 @@ class ForumDatabaseCommunicator:
                 ''' %(new_id)
             )
             self.database_connection.commit()
-            self.message = '''{"Message" : "Sign up 0", "Datas" : ""}'''
+            t = '''{"Message" : "Sign up 0", "Datas" : ""}'''
+            self.message = json.dumps(t)
         else:
-            self.message = '''{'Message" : "Sign up 1", "Datas" : ""}'''
+            t = '''{'Message" : "Sign up 1", "Datas" : ""}'''
+            self.message = json.dumps(t)
         
         #print(self.message)
         return self.message
